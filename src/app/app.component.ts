7c171a24d7f2e5dfeb2c0d79f4df3668241c1fc7
leidysantacruz <leidyjohanasantacruzdaza03@gmail.com>
2024-08-12 16:56:32 -0500

    componentess-databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LikeComponent } from './like/like.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LikeComponent,UserFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/like/like.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-like',
+  standalone: true,
+  imports: [],
+  template: '<p>like works!</p>',
+ styles:''
+})
+export class LikeComponent {
+
+}
